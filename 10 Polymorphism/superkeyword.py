343cc608af426a7830cc4f9dc77d189177dae342
shubham-pattewar <sspattewar2004@gmail.com>
2025-10-13 23:08:47 +0530

    New

--- 10 Polymorphism/superkeyword.py
+++ 10 Polymorphism/superkeyword.py
@@ -12,3 +12,17 @@ class Child(Parent):
 x = Child("Hello, and welcome!")
 
 x.printmessage()
+
+
+class Animal:
+    def __init__(self):
+        print("This is the Animal class")
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()   # calls the parent class constructor
+        print("This is the Dog class")
+
+# Create object
+d = Dog()
+
